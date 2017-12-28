e08904f18d8c8a93284d8988bd7ab0d6e90292e0
Lavie <xu.lavie@gmail.com>
2017-12-28 16:29:10 +0800

    fix some bugs

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    username = forms.CharField(label='学号')
+    username = forms.Field(label='学号')
     email = forms.EmailField(label='邮箱')
     password = forms.CharField(label='密码', widget=forms.PasswordInput)
     name = forms.CharField(label='姓名', max_length=20)
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from .models import UserProfile
 
 
 # Create your views here.
@@ -14,14 +15,16 @@ def register(request):
         form = UserForm(data=request.POST)
         if form.is_valid():
             new_user = User()
-            new_user.username = form['username']
-            new_user.set_password(form['password'])
-            new_user.email = form['email']
+            new_user.username = form.cleaned_data['username']
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.email = form.cleaned_data['email']
             new_user.save()
-            profile = new_user.get_profile()
-            profile.name = form['name']
+            profile = UserProfile()
+            profile.user_id = new_user.id
+            profile.name = form.cleaned_data['name']
+            profile.save()
             authenticated_user = authenticate(username=new_user.username,
-                                              password=request.POST['password'])
+                                              password=form.cleaned_data['password'])
             login(request, authenticated_user)
             return HttpResponseRedirect(reverse('tasks:index'))
     context = {'form': form}
